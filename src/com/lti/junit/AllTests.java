f1e0870ab04b2f8cdc362735890ae094b237eb13
Vshadmin <manish.maan@lntinfotech.com>
2019-06-26 18:06:24 +0530

    JUnit

---
+++ src/com/lti/junit/AllTests.java
@@ -0,0 +1,11 @@
+package com.lti.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ SuiteTest1.class, SuiteTest2.class })
+public class AllTests {
+
+}
